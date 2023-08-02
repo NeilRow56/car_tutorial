88c40a96da95629b18de4aec8f15b596d8a0e1a5
NeilRow56 <neilrowland56@gmail.com>
2023-08-02 09:34:14 +0100

    Added Search Manufacturers filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
-import { Hero } from '@/components'
+import { CustomFilter, Hero, Searchbar } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
      <Hero />
+     <div className='mt-12 padding-y padding-x max-width' id="discover">
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+          <p>
+            Explore the cars you may like
+          </p>
+        </div>
+        <div className='home__filters'>
+          <Searchbar />
+          <div className='home__filter-container'>
+          <CustomFilter  />
+          <CustomFilter  />
+          </div>
+        </div>
+     </div>
     </main>
   )
 }
---
+++ components/CustomFilter.tsx
@@ -0,0 +1,12 @@
+
+import { CustomFilterProps } from "@/types";
+
+const CustomFilter = () => {
+  return (
+    <div>
+      Custom Filter 
+    </div>
+  )
+}
+
+export default CustomFilter
